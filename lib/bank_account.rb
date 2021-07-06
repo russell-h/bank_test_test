4d84d5a0110e23ba52dcdeffa6563b26ca58012e
Russell Horth <russellhorth@googlemail.com>
2021-07-06 11:37:41 +0100

    float rounded to 2 decimal places

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,13 +18,15 @@ class BankAccount
 
 
   def deposit(amount, deposit_date)
-    new_deposit_date = deposit_date.to_s
-    @account_arr << "#{new_deposit_date.insert(2,'/').insert(5,'/')} || #{amount} ||      ||#{@balance += amount}"
+    amount = amount.to_f.round(2)
+    deposit_date = deposit_date.to_s
+    @account_arr << "#{deposit_date.insert(2,'/').insert(5,'/')} || #{amount} ||      ||#{@balance += amount}"
   end
 
   def withdraw(amount, withdrawal_date)
-    new_withdraw_date = withdrawal_date.to_s
-    @account_arr << "#{new_withdraw_date.insert(2,'/').insert(5,'/')} ||     ||  #{amount} ||#{@balance -= amount}"
+    amount = amount.to_f.round(2)
+    withdraw_date = withdrawal_date.to_s
+    @account_arr << "#{withdraw_date.insert(2,'/').insert(5,'/')} ||     ||  #{amount} ||#{@balance -= amount}"
   end
 
   def statement
